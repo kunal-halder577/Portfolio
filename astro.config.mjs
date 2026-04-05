68cdf75a2e8405f7e105828447a138814f3223dc
Kunal <kunalhalder577@gmail.com>
2026-04-05 23:24:59 +0530

    feat: added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'https://kunalhalder.site',
   integrations: [
     react(),
     tailwind(),
+    sitemap(),
   ],
 });
